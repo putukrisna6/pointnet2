b983067c531434fc0231dc0eb130007558b2c7c6
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-05-09 20:11:39 +0700

    fix: shape

--- models/pointnet2_cls_ssg.py
+++ models/pointnet2_cls_ssg.py
@@ -44,7 +44,7 @@ def get_model(point_cloud, is_training, bn_decay=None):
     net = tf.reshape(l3_points, [batch_size, -1])
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp1')
-    net = tf_util.fully_connected(net, 256, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
+    net = tf_util.fully_connected(net, num_point, bn=True, is_training=is_training, scope='fc2', bn_decay=bn_decay)
     net = tf_util.dropout(net, keep_prob=0.5, is_training=is_training, scope='dp2')
     net = tf_util.fully_connected(net, 40, activation_fn=None, scope='fc3')
 
