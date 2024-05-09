74ddfce4b8335f9f1b78f269c2ba70ae7d8ec2ba
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-05-09 21:36:21 +0700

    style(model): log info

--- utils/tf_util.py
+++ utils/tf_util.py
@@ -527,7 +527,7 @@ def batch_norm_template(inputs, is_training, scope, moments_dims_unused, bn_deca
   """
   bn_decay = bn_decay if bn_decay is not None else 0.9
 
-  print("INFO: scope: ", scope)
+  print("INFO: is_training: ", is_training)
 
   return tf.keras.layers.BatchNormalization(
     axis=-1 if data_format == "NHWC" else 1,
