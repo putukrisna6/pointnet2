c928d0274605e9d9e4e4e70e88082aa023081c0f
Putu Krisna Andyartha <putukrisna6@gmail.com>
2024-05-09 21:26:11 +0700

    style(model): log info

--- train.py
+++ train.py
@@ -211,6 +211,10 @@ def train_one_epoch(sess, ops, train_writer):
         feed_dict = {ops['pointclouds_pl']: cur_batch_data,
                      ops['labels_pl']: cur_batch_label,
                      ops['is_training_pl']: is_training,}
+        
+        print("INFO: feed: ", feed_dict)
+        return
+
         summary, step, _, loss_val, pred_val = sess.run([ops['merged'], ops['step'],
             ops['train_op'], ops['loss'], ops['pred']], feed_dict=feed_dict)
         
